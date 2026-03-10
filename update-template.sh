b259d712dab5c9e990efc6059f78db5786c720c1
OverLessArtem <Artemfreedom@tuta.io>
2026-03-10 15:29:49 +0500

    fix typo

--- update-template.sh
+++ update-template.sh
@@ -13,7 +13,7 @@ if [ "$LATEST_VERSION" = "$CURRENT_VERSION" ]; then
     exit 0
 fi
 
-echo "Updating: $CURRENT_VERSION -> $LATEST_VERSION"
+echo "Update found: $CURRENT_VERSION -> $LATEST_VERSION"
 
 URL_X86="https://github.com/$REPO/releases/download/${LATEST_VERSION}/helium-${LATEST_VERSION}-x86_64_linux.tar.xz"
 URL_ARM="https://github.com/$REPO/releases/download/${LATEST_VERSION}/helium-${LATEST_VERSION}-arm64_linux.tar.xz"
@@ -25,14 +25,15 @@ echo "Calculating arm64 checksum..."
 CHK_ARM=$(curl -L -s "$URL_ARM" | sha256sum | awk '{print $1}')
 
 if [ -z "$CHK_X86" ] || [ -z "$CHK_ARM" ]; then
-    echo "Error: Could not calculate checksums!"
+    echo "Error: Failed to fetch checksums."
     exit 1
 fi
 
 sed -i "s/^version=.*/version=$LATEST_VERSION/" "$TPL"
 sed -i "s/^revision=.*/revision=1/" "$TPL"
-
 sed -i "/x86_64)/,/;;/ s/_chk=\".*\"/_chk=\"$CHK_X86\"/" "$TPL"
 sed -i "/aarch64)/,/;;/ s/_chk=\".*\"/_chk=\"$CHK_ARM\"/" "$TPL"
+
+echo "NEW_VERSION=$LATEST_VERSION" >> $GITHUB_ENV
 
 echo "### Success! Template updated to $LATEST_VERSION"
